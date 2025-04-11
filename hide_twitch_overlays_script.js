eb70e8e682fd316cef0a43966c4030a9f7ed7296
Guillaume Desktop <robertohelper@outlook.com>
2025-04-12 01:16:03 +0200

    feature: script now works when switching twitch links

--- hide_twitch_overlays_script.js
+++ hide_twitch_overlays_script.js
@@ -8,42 +8,61 @@
 // @run-at       document-idle
 // @grant        none
 // ==/UserScript==
-
-(function() {
+(function(history) {
     'use strict';
-    const targetNode = document.querySelector('.twilight-main .scrollable-area');
-    const config = { attributes: true, childList: true, subtree: true };
+    function onContentLoaded() {
 
-    const callback = (mutationList, observer) => {
-        for (const mutation of mutationList) {
-            if (mutation.type === "childList" && mutation.addedNodes) {
-                for (const node of mutation.addedNodes) {
-                    if (!node.className) {
-                        continue;
-                    }
-                    if (node.classList.contains('extension-view__iframe')) {
-                        node.remove();
-                        observer.disconnect();
+        const callback = (mutationList, observer) => {
+            for (const mutation of mutationList) {
+                if (mutation.type === "childList" && mutation.addedNodes) {
+                    for (const node of mutation.addedNodes) {
+                        if (!node.className) {
+                            continue;
+                        }
+                        if (node.classList.contains('extension-view__iframe')) {
+                            node.remove();
+                            observer.disconnect();
+                        }
                     }
                 }
             }
-        }
-    };
+        };
 
-    const observer = new MutationObserver(callback);
+        const targetNode = document.querySelector('.twilight-main .scrollable-area');
+        const observer = new MutationObserver(callback);
+        const config = { childList: true };
 
-    observer.observe(targetNode, config);
+        observer.observe(targetNode, config);
 
-    window.addEventListener("beforeunload", () => {
-        // no clue if this actually works
-        observer.disconnect();
-    });
+        const interval = setInterval(() => {
+            const osrs_pop_ups = document.querySelector(".Layout-sc-1xcs6mc-0.djGvAr.video-size.passthrough-events");
+            if (osrs_pop_ups) {
+                osrs_pop_ups.remove();
+                clearInterval(interval);
+            }
+        }, 1_000);
 
-    const interval = setInterval(() => {
-        const osrs_pop_ups = document.querySelector(".Layout-sc-1xcs6mc-0.djGvAr.video-size.passthrough-events");
-        if (osrs_pop_ups) {
-            osrs_pop_ups.remove();
+        window.addEventListener("beforeunload", () => {
+            // no clue if this actually works
+            observer.disconnect();
             clearInterval(interval);
+        });
+    }
+
+    var pushState = history.pushState;
+    history.pushState = function(state) {
+        if (typeof history.onpushstate == "function") {
+            history.onpushstate({state: state});
         }
-    }, 1_000);
-})();
+        onContentLoaded();
+        return pushState.apply(history, arguments);
+    }
+
+    if (document.readyState === "complete" || document.readyState === "loaded" || document.readyState === "interactive") {
+        onContentLoaded();
+    } else {
+        window.addEventListener("DOMContentLoaded", function() {
+            onContentLoaded();
+        });
+    }
+})(window.history);
